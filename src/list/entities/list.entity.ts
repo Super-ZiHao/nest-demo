ca81afa2f4e7c1801941646cec3c9c83c62c82de
v_hzihhuang <v_hzihhuang@tencent.com>
2022-11-29 17:48:31 +0800

    perf: 实体使用

--- src/list/entities/list.entity.ts
+++ src/list/entities/list.entity.ts
@@ -1,16 +1,38 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  Generated,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 
 @Entity()
 export class List {
+  @Generated('uuid')
+  uuid: string;
+
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column()
+  @Column({
+    type: 'varchar',
+    length: 254,
+  })
   name: string;
 
-  @Column()
+  @Column({ select: true, comment: '注释', nullable: true })
   password: string;
 
   @Column()
   age: number;
+
+  @CreateDateColumn({ type: 'timestamp' })
+  createTime: Date;
+
+  @Column({
+    type: 'enum',
+    enum: [1, 2, 3],
+    default: 1,
+  })
+  top: number;
 }
